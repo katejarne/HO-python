ac04b4862c37d770b8af95f253420ef8085a3cd4
Cecilia Jarne <katejarne@gmail.com>
2018-02-27 10:40:25 -0300

    Update ejemplo.py

--- ejemplos/ejemplo.py
+++ ejemplos/ejemplo.py
@@ -1,8 +1,8 @@
 
 # scrip to count... and sum them as a list
 
-a= range(1,1001,3) #defined as a list
-b= range(1,1001,5)
+a= range(0,1000,3) #defined as a list
+b= range(0,1000,5)
 c = set(a) | set(b) #redifined as a set and union
 print c
 print sum(c)
